f38e63cf8bdccc5ae79c7cf82105a12403ba8bfc
greysonmitra <greyson.mitra@outlook.com>
2015-10-07 09:56:30 -0600

    Added a GUI that shows list spot in an Array

--- src/lists/controller/ArrayListController.java
+++ src/lists/controller/ArrayListController.java
@@ -1,9 +1,29 @@
 package lists.controller;
 
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
 public class ArrayListController
 {
+	private ArrayList<String> firstWords;
+	
+	public ArrayListController()
+	{
+		firstWords = new ArrayList<String>();
+	}
+	
 	public void start()
 	{
-		
+		firstWords.add("Mama Dada");
+		firstWords.add("Buhbuhbuhbuhbuhbuh");
+		showList();
+	}
+	
+	private void showList()
+	{
+		for(int spot = 0; spot < firstWords.size(); spot++)
+		{
+			JOptionPane.showMessageDialog(null, firstWords.get(spot) + " is at spot #" + spot);
+		}
 	}
 }
